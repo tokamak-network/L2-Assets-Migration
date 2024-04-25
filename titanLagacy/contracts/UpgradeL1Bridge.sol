d6eda92770f5974367eab54716a084303783391c
MarkInc3 <mark.k@onther.io>
2024-04-25 12:19:57 +0900

    feat : force withdraw update

--- titanLagacy/contracts/UpgradeL1Bridge.sol
+++ titanLagacy/contracts/UpgradeL1Bridge.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.9;
 
 /* Interface Imports */
-import { IL1StandardBridge } from "L2-Assets-Migration/sub/tokamak-titan/packages/tokamak/contracts/contracts/L1/messaging/IL1StandardBridge.sol";
-import { IL1ERC20Bridge } from "L2-Assets-Migration/sub/tokamak-titan/packages/tokamak/contracts/contracts/L1/messaging/IL1ERC20Bridge.sol";
-import { IL2ERC20Bridge } from "L2-Assets-Migration/sub/tokamak-titan/packages/tokamak/contracts/contracts/L2/messaging/IL2ERC20Bridge.sol";
+import { IL1StandardBridge } from "sub/tokamak-titan/packages/tokamak/contracts/L1/messaging/IL1StandardBridge.sol";
+import { IL1ERC20Bridge } from "sub/tokamak-titan/packages/tokamak/contracts/contracts/L1/messaging/IL1ERC20Bridge.sol";
+import { IL2ERC20Bridge } from "sub/tokamak-titan/packages/tokamak/contracts/contracts/L2/messaging/IL2ERC20Bridge.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // /* Library Imports */
-import { CrossDomainEnabled } from "L2-Assets-Migration/sub/tokamak-titan/packages/tokamak/contracts/contracts/libraries/bridge/CrossDomainEnabled.sol";
-import { Lib_PredeployAddresses } from "L2-Assets-Migration/sub/tokamak-titan/packages/tokamak/contracts/contracts/libraries/constants/Lib_PredeployAddresses.sol";
+import { CrossDomainEnabled } from "sub/tokamak-titan/packages/tokamak/contracts/contracts/libraries/bridge/CrossDomainEnabled.sol";
+import { Lib_PredeployAddresses } from "sub/tokamak-titan/packages/tokamak/contracts/contracts/libraries/constants/Lib_PredeployAddresses.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -21,13 +21,15 @@ import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.s
  *
  */
 contract L1StandardBridge is IL1StandardBridge, CrossDomainEnabled {
-    using SafeERC20 for IERC20;
+     using SafeERC20 for IERC20;
 
     /********************************
      * External Contract References *
      ********************************/
 
     address public l2TokenBridge;
+    
+    
 
     // Maps L1 token to L2 token to balance of the L1 token deposited
     mapping(address => mapping(address => uint256)) public deposits;
@@ -91,7 +93,7 @@ contract L1StandardBridge is IL1StandardBridge, CrossDomainEnabled {
         address _to,
         uint32 _l2Gas,
         bytes calldata _data
-    ) external payable paused {
+    ) external payable {
         _initiateETHDeposit(msg.sender, _to, _l2Gas, _data);
     }
 
@@ -139,7 +141,7 @@ contract L1StandardBridge is IL1StandardBridge, CrossDomainEnabled {
         uint256 _amount,
         uint32 _l2Gas,
         bytes calldata _data
-    ) external virtual onlyEOA paused {
+    ) external virtual onlyEOA {
         _initiateERC20Deposit(_l1Token, _l2Token, msg.sender, msg.sender, _amount, _l2Gas, _data);
     }
 
@@ -266,16 +268,74 @@ contract L1StandardBridge is IL1StandardBridge, CrossDomainEnabled {
     // closer
     modifier onlyCloser() {
         if(msg.sender != closer) revert("Only Closer");
+        _;
     }
     modifier paused() {
-        if(paused) revert("Paused L1StandardBridge");
+        if(active) revert("Paused L1StandardBridge");
+        _;
+    }
+    struct AssetsParam {
+        address claimer;
+        bytes32 key;
+    }
+    struct ClaimParam {
+        address token;
+        uint amount;
     }
 
+    mapping(bytes32 => address) private assets; // hased(token, amount) => Account
     address constant private closer = address(0);  // msg.sender pauser same 
-    bool public paused = false;
+    bool private active = false;
 
-    function close() external onlyCloser {
-        paused = !paused;
+    function doActive() external onlyCloser {
+        active = !active;
     }
 
+    function registry(AssetsParam[] calldata _params) external onlyCloser {
+        for (uint i = 0; i < _params.length; i++) {
+            if(assets[_params[i].key] != address(0))
+                assets[_params[i].key] = _params[i].claimer;
+        }
+    }
+    
+    function verifyRegistry(AssetsParam[] calldata _params) external view returns (AssetsParam memory) {
+        for (uint i = 0; i < _params.length; i++) {
+            if(assets[_params[i].key] == address(0))
+                return _params[i];
+        }
+        return AssetsParam(address(0), bytes32(0));
+    }
+
+    function editMigration(bytes32 _old, bytes32 _new, address claimer) external onlyCloser {
+        assets[_old] = address(0);
+        assets[_new] = claimer; 
+    }
+
+    function migrattionClaim(address _token, uint _amount) external {
+        _migrattionClaim(_token, _amount);
+    }
+
+    function migrattionClaimAll(ClaimParam[] calldata _params) external {
+        for(uint i = 0; i < _params.length; i++)
+            _migrattionClaim(_params[i].token, _params[i].amount);
+    }
+
+    function generateKey(address _token, address _claimer, uint _amount) external pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_token, _claimer, _amount));
+    }
+
+    function _migrattionClaim(address _token, uint _amount) internal {
+        address claimer = assets[keccak256(abi.encodePacked(_token, msg.sender, _amount))];
+        if (claimer != msg.sender || claimer == address(0))
+            revert("not claimer");
+        
+        assets[keccak256(abi.encodePacked(_token, msg.sender, _amount))] = address(0);
+
+        if(_token == address(0)){
+            (bool success, ) = msg.sender.call{ value: _amount }(new bytes(0));
+            require(success, "TransferHelper::safeTransferETH: ETH transfer failed");
+        }
+        else 
+            IERC20(_token).safeTransfer(msg.sender, _amount);
+    }
 }
