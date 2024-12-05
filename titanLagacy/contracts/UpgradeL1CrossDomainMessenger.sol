88f6387d68e377d442686f466038b584d5eaad09
DevUreak <rlaquddlf3204@gmail.com>
2024-12-05 15:17:37 +0900

    feat : add unpause

--- titanLagacy/contracts/UpgradeL1CrossDomainMessenger.sol
+++ titanLagacy/contracts/UpgradeL1CrossDomainMessenger.sol
@@ -5,4 +5,11 @@ import { L1CrossDomainMessenger } from "./L1CrossDomainMessenger.sol";
 
 contract UpgradeL1CrossDomainMessenger is L1CrossDomainMessenger {
     
+    /**
+    * unPause the contract
+    */
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
 }
