924a633a891c94a7ba875b706b7e0649d1c5b9e7
HyukSang <zzooppii@gmail.com>
2024-12-23 22:54:30 +0900

    Update AllclaimTest-forking-sepolia.js

--- titanLagacy/scripts/new_gen/test/AllclaimTest-forking-sepolia.js
+++ titanLagacy/scripts/new_gen/test/AllclaimTest-forking-sepolia.js
@@ -44,6 +44,13 @@ async function claimTest() {
 
 
     const L1BridgeProxyAddr = "0x1F032B938125f9bE411801fb127785430E7b3971"
+    
+    let l1TON = "0xa30fe40285B8f5c0457DbC3B7C8A280373c40044"
+    let l1ETH = "0x0000000000000000000000000000000000000000"
+    let l1USDT = "0x42d3b260c761cD5da022dB56Fe2F89c4A909b04A"
+    let l1TOS = "0xFF3Ef745D9878AfE5934Ff0b130868AFDDbc58e8"
+    let l1USDC = "0x693a591A27750eED2A0e14BC73bB1F313116a1cb"
+
     const oneETH = ethers.utils.parseUnits('1', 18)
 
     
@@ -53,40 +60,6 @@ async function claimTest() {
         UpgradeL1Bridge_ABI.abi,
         deployer
     )
-
-    let positionAddress = "0x1a7dFF905E30d36578b6b2aC46089dEc51531067"
-
-    let readFile1 ='./data/new-generate-assets.json'
-    let assets
-    if (await fs.existsSync(readFile1)) assets = JSON.parse(await fs.readFileSync(readFile1));
-    console.log(assets)
-    console.log(assets.length)
-
-
-    //onwer1
-    let TONHash = "0xd9f5d9d671fe106bbfb7a09190ad3442d6b09a407c1a4faa248662881cd4f887"
-    let TONAmount = ethers.BigNumber.from("13937332320000000100000")
-    let l1TON = "0xa30fe40285B8f5c0457DbC3B7C8A280373c40044"
-
-    let ETHHash = "0x964d8433b7db6d963386e06608b083fee40ad1a7b12ee75d081c878dc95431db"
-    let ETHAMount = ethers.BigNumber.from("609983199953732915")
-    let l1ETH = "0x0000000000000000000000000000000000000000"
-
-    let USDTHash = "0xada3b093f3324e18dc1793fcf1c7cf6b72f02ff7160e4a79ea5619f75da20988"
-    let USDTAMount = ethers.BigNumber.from("2927000220")
-    let l1USDT = "0x42d3b260c761cD5da022dB56Fe2F89c4A909b04A"
-
-    let TOSHash = "0x44ec157ed78cef8f4f244356a5a84c616a44dc27180a4fcd1189890afc433ff8"
-    let TOSAMount = ethers.BigNumber.from("10000000000000000000")
-    let l1TOS = "0xFF3Ef745D9878AfE5934Ff0b130868AFDDbc58e8"
-
-    //owner2
-    let USDCHash = "0xc93c02acea5f89bec1881615a84caa9c47c217877ef110a9322a7f5b1939b0c4"
-    let USDCAMount = ethers.BigNumber.from("8999998")
-    let l1USDC = "0x693a591A27750eED2A0e14BC73bB1F313116a1cb"
-
-    let NoHash = "0x44ec157ed78cef8f4f244356a5a84c616a44dc27180a4fcd1189890afc433000"
-    
 
     let TON = new ethers.Contract(
         l1TON,
@@ -111,7 +84,63 @@ async function claimTest() {
         TON_ABI.abi,
         deployer
     )
-    
+
+    let positionAddress = "0x1a7dFF905E30d36578b6b2aC46089dEc51531067"
+
+    let readFile1 ='./data/new-generate-assets.json'
+    let assets
+    if (await fs.existsSync(readFile1)) assets = JSON.parse(await fs.readFileSync(readFile1));
+    // console.log(assets)
+    // console.log(assets.length)
+    let tokenAddr
+    let Hash
+    let Account
+    let Amount
+    let getAccount
+    let getAddress
+    let testZeroAddr = "0x0000000000000000000000000000000000000000";
+
+    for(let i = 0; i < assets.length; i++) {
+        tokenAddr = assets[i].l1Token
+        console.log("tokenAddr : ", tokenAddr);
+        // console.log(assets[i].data.length);
+        for(let j = 0; j < assets[i].data.length; j++) {
+            Account = assets[i].data[j].claimer
+            Amount = ethers.BigNumber.from(assets[i].data[j].amount)
+            Hash = assets[i].data[j].hash
+            
+            await ethers.provider.send('hardhat_impersonateAccount', [
+                Account
+            ])
+            getAccount = await ethers.getSigner(Account);
+            
+            if(i == 0 && j ==0){
+                console.log("Account : ", getAccount.address)
+                console.log("Amount : ", Amount)
+                console.log("Hash : ", Hash)
+            }
+            getAddress = await UpgradeL1BridgeLogic.connect(getAccount).getForcePosition(Hash)
+            
+            if(getAddress == testZeroAddr) {
+                console.log("error :", Hash)
+                break;
+            }
+
+            await UpgradeL1BridgeLogic.connect(getAccount).forceWithdrawClaim(
+                positionAddress,
+                Hash,
+                tokenAddr,
+                Amount
+            )
+
+            getAddress = await UpgradeL1BridgeLogic.connect(getAccount).gb(Hash)
+            
+            if(getAddress == Account) {
+                console.log("gb error :", Hash)
+                break;
+            }
+        }
+    }
 
 
     // let getAddress = await UpgradeL1BridgeLogic.connect(tester).getForcePosition(TONHash)
