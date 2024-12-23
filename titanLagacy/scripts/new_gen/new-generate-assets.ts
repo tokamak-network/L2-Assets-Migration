b026b37fb765cc5abba94f72bb6a2dcfa5fd6393
HyukSang <zzooppii@gmail.com>
2024-12-23 14:32:42 +0900

    Update new-generate-assets.ts

--- titanLagacy/scripts/new_gen/new-generate-assets.ts
+++ titanLagacy/scripts/new_gen/new-generate-assets.ts
@@ -15,21 +15,58 @@ const main = async () => {
     
     console.log(assetsData.length)
 
-    let l1TON = "0xa30fe40285B8f5c0457DbC3B7C8A280373c40044"
-    let l2TON = "0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2"
+    let l1TON;
+    let l2TON;
     let tokenNameTON = "Tokamak Network Token"
-    let l1TOS = "0xFF3Ef745D9878AfE5934Ff0b130868AFDDbc58e8"
-    let l2TOS = "0xD08a2917653d4E460893203471f0000826fb4034"
+    let l1TOS;
+    let l2TOS;
     let tokenNameTOS = "TONStarter"
-    let l1USDC = "0x693a591A27750eED2A0e14BC73bB1F313116a1cb"
-    let l2USDC = "0xFF3Ef745D9878AfE5934Ff0b130868AFDDbc58e8"
+    let l1USDC;
+    let l2USDC;
     let tokenNameUSDC = "USD Coin"
-    let l1USDT = "0x42d3b260c761cD5da022dB56Fe2F89c4A909b04A"
-    let l2USDT = "0x79E0d92670106c85E9067b56B8F674340dCa0Bbd"
+    let l1USDT;
+    let l2USDT;
     let tokenNameUSDT = "Tether USD"
-    let l1ETH = "0x0000000000000000000000000000000000000000"
-    let l2ETH = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"
+    let l1ETH;
+    let l2ETH;
     let tokenNameETH = "Ether"
+
+    if(hre.network.name == "titansepolia") {
+        //sepolia
+        l1TON = "0xa30fe40285B8f5c0457DbC3B7C8A280373c40044"
+        l2TON = "0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2"
+        l1TOS = "0xFF3Ef745D9878AfE5934Ff0b130868AFDDbc58e8"
+        l2TOS = "0xD08a2917653d4E460893203471f0000826fb4034"
+        l1USDC = "0x693a591A27750eED2A0e14BC73bB1F313116a1cb"
+        l2USDC = "0xFF3Ef745D9878AfE5934Ff0b130868AFDDbc58e8"
+        l1USDT = "0x42d3b260c761cD5da022dB56Fe2F89c4A909b04A"
+        l2USDT = "0x79E0d92670106c85E9067b56B8F674340dCa0Bbd"
+        l1ETH = "0x0000000000000000000000000000000000000000"
+        l2ETH = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"
+    } else if (hre.network.name == "titan") {
+        //mainnet
+        l1TON = "0x2be5e8c109e2197d077d13a82daead6a9b3433c5"
+        l2TON = "0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2"
+
+        l1TOS = "0x409c4D8cd5d2924b9bc5509230d16a61289c8153"
+        l2TOS = "0xD08a2917653d4E460893203471f0000826fb4034"
+
+        l1USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+        l2USDC = "0x46BbbC5f20093cB53952127c84F1Fbc9503bD6D9"
+
+        l1USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7"
+        l2USDT = "0x2aCC8EFEd68f07DEAaD37f57A189677fB5655B46"
+
+        l1ETH = "0x0000000000000000000000000000000000000000"
+        l2ETH = "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"
+    }
+
+    if(l1ETH != "0x0000000000000000000000000000000000000000") {
+        console.log("network error")
+    }
+
+
+    //mainnet
 
     const innerTON = {
         l1Token: l1TON,
